c95fcd6a5a252f83c3b6b2e6cbe6fb0e36d9b071
Neal Lubin <maniator@users.noreply.github.com>
2018-03-05 10:22:37 -0500

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,7 @@ describe('undefined is not a function?', () => {
   test('it is now', () => {
     expect(typeof undefined).toBe('function');
   });
+  test('it should return itself', () => {
+    expect(undefined()).toBe(undefined);
+  });
 });
